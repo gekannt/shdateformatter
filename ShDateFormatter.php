2005c39eb5e2cb3a313ad5e9cbe78b5347413afa
Hans Lauer <contact@alexshulzhenko.ru>
2016-09-09 23:14:26 +0300

    first commit

---
+++ ShDateFormatter.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ *   Created on: Sep 9, 2016   5:46:38 PM
+ *   @copyright (c) Apotheke - All Rights Reserved
+ *  2016, Alexander Shulzhenko,  contact@alexshulzhenko.ru
+ */
+
+namespace Helloworld;
+
+class ShDateFormatter
+{
+
+    public static function hoho()
+    {
+        die('hjnj');
+    }
+
+}
